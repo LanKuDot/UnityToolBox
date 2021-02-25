f32d3d506b408db3d7705dd5c81ba25cf80f059e
LanKuDot <airlanser@gmail.com>
2021-02-25 10:37:37 +0800

    Add MonoSingleton

---
+++ MonoSingleton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Create the singleton instance of MonoBehaviour class
+/// </summary>
+public class MonoSingleton<T> : MonoBehaviour
+{
+    public static T Instance { get; private set; }
+
+    protected void Awake()
+    {
+        Instance = GetComponent<T>();
+    }
+}
