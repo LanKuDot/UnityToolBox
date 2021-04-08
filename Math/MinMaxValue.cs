a8a8c9035646c5af0587bd8579643a86c29b2390
LanKuDot <airlanser@gmail.com>
2021-04-08 15:48:26 +0800

    Add MinMaxValue

---
+++ Math/MinMaxValue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class MinMaxValue
+{
+    [SerializeField]
+    private float _min;
+    [SerializeField]
+    private float _max;
+
+    public float min => _min;
+    public float max => _max;
+
+    /// <summary>
+    /// Check if the input value is in the range of the min-max value [both included]
+    /// </summary>
+    public bool IsWithin(float value)
+    {
+        return value >= _min && value <= _max;
+    }
+}
