453736f3ca0ce67fcc09dfef288a3b9f2df35bd9
LanKuDot <airlanser@gmail.com>
2021-04-12 17:37:26 +0800

    Add TextColorPairText

---
+++ UI/Text/TextColorPairText.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace GamePlay.UI
+{
+    /// <summary>
+    /// Display the text and related color
+    /// </summary>
+    public class TectColorPairText : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI _text;
+        [SerializeField]
+        private TextColorPair[] _textColorPairs;
+
+        public void Show(int index)
+        {
+            _text.text = _textColorPairs[index].text;
+            _text.color = _textColorPairs[index].color;
+        }
+    }
+
+    [Serializable]
+    public class TextColorPair
+    {
+        public string text;
+        public Color color;
+    }
+}
