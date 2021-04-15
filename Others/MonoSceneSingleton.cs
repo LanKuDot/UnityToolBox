12fade0e2dcc32430cf3e0e5e466f34c472373b9
LanKuDot <airlanser@gmail.com>
2021-04-15 16:31:09 +0800

    Create MonoSceneSingleton and MonoStatic to replace MonoSingleton

---
+++ Others/MonoSceneSingleton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Create the singleton instance of class MonoBehaviour for entire game
+/// </summary>
+public class MonoSceneSingleton<T> : MonoStatic<T>
+    where T : MonoBehaviour
+{
+    protected new void Awake()
+    {
+        if (Instance == null) {
+            base.Awake();
+            DontDestroyOnLoad(gameObject);
+        } else if (Instance.gameObject != gameObject) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Others/MonoStatic.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Create the static instance of MonoBehaviour class
+/// </summary>
+public class MonoStatic<T> : MonoBehaviour
+    where T : MonoBehaviour
+{
+    public static T Instance { private set; get; }
+
+    protected void Awake()
+    {
+        if (Instance != null)
+            Debug.Log("There has more than one class instance for MonoStatic.")
+
+        Instance = GetComponent<T>();
+    }
+}
