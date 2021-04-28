5c51aca47dd5b07e0a8775468b9465f4727b33cc
LanKuDot <airlanser@gmail.com>
2021-04-28 15:13:02 +0800

    MinMaxValue: Add constructor and Clamp()

--- Math/MinMaxValue.cs
+++ Math/MinMaxValue.cs
@@ -12,6 +12,25 @@ public class MinMaxValue
     public float min => _min;
     public float max => _max;
 
+    public MinMaxValue()
+    {}
+
+    public MinMaxValue(float min, float max)
+    {
+        _min = min;
+        _max = max;
+    }
+
+    /// <summary>
+    /// Clamp the value within the range of min-max value
+    /// </summary>
+    /// <param name="value">The value to be clamped</param>
+    /// <returns>The clamped value</returns>
+    public float Clamp(float value)
+    {
+        return Mathf.Clamp(value, _min, _max);
+    }
+
     /// <summary>
     /// Check if the input value is in the range of the min-max value [both included]
     /// </summary>
