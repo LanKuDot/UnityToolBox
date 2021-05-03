138346160536360eb09ca649d897a719eac0e0dc
LanKuDot <airlanser@gmail.com>
2021-05-03 16:00:16 +0800

    Add StageState

---
+++ System/StageState/State.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+
+namespace CommonToolBox.StageState
+{
+    /// <summary>
+    /// The basic component of the stage state
+    /// </summary>
+    public abstract class State
+    {
+        protected readonly GamePlayManager gamePlayManager;
+
+        protected State(GamePlayManager gamePlayManager)
+        {
+            this.gamePlayManager = gamePlayManager;
+        }
+
+        /// <summary>
+        /// Things to do when the state starts
+        /// </summary>
+        public virtual IEnumerator OnStart()
+        {
+            yield break;
+        }
+
+        /// <summary>
+        /// Things to do when the state ends
+        /// </summary>
+        public virtual IEnumerator OnEnd()
+        {
+            yield break;
+        }
+
+        /// <summary>
+        /// Get the next state of this state
+        /// </summary>
+        /// <returns>The next state</returns>
+        public virtual State GetNextState()
+        {
+            return null;
+        }
+    }
+}
---
+++ System/StageState/StateMachine.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+
+namespace CommonToolBox.StageState
+{
+    /// <summary>
+    /// Control the transition between states
+    /// </summary>
+    public sealed class StateMachine
+    {
+        /// <summary>
+        /// The manager providing the main functions
+        /// </summary>
+        private readonly GamePlayManager _gamePlayManager;
+        /// <summary>
+        /// The current state
+        /// </summary>
+        private State _curState;
+
+        public StateMachine(GamePlayManager gamePlayManager)
+        {
+            _gamePlayManager = gamePlayManager;
+        }
+
+        /// <summary>
+        /// Start the state machine
+        /// </summary>
+        public void StartMachine()
+        {
+            _curState = new StageStartCase(_gamePlayManager);
+            _gamePlayManager.StartCoroutine(_curState.OnStart());
+        }
+
+        /// <summary>
+        /// Switch to the next state
+        /// </summary>
+        public void NextState()
+        {
+            if (_curState == null)
+                return;
+
+            _gamePlayManager.StartCoroutine(StateTransition());
+        }
+
+        private IEnumerator StateTransition()
+        {
+            yield return _curState.OnEnd();
+            _curState = _curState.GetNextState();
+            if (_curState != null)
+                yield return _curState.OnStart();
+        }
+    }
+}
