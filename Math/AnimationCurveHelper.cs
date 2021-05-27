86e981ff24062cb6f6d4a5a68b03b57dd63016f6
LanKuDot <airlanser@gmail.com>
2021-05-27 15:42:52 +0800

    Add AnimationCurveHelper

---
+++ Math/AnimationCurveHelper.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace LanKuDot.UnityToolBox.Math
+{
+    public class AnimationCurveHelper
+    {
+        private readonly AnimationCurve _curve;
+        private readonly float _minTime;
+        private readonly float _maxTime;
+
+        public AnimationCurveHelper(AnimationCurve curve)
+        {
+            _curve = curve;
+            _minTime = _curve[0].time;
+            _maxTime = _curve[_curve.length - 1].time;
+        }
+
+        /// <summary>
+        /// Evaluate the animation curve within the time period specified in the curve
+        /// </summary>
+        /// <param name="time">The time</param>
+        /// <returns>The value evaluated in the range of the curve</returns>
+        public float EvaluateClamped(float time)
+        {
+            return _curve.Evaluate(Mathf.Clamp(time, _minTime, _maxTime));
+        }
+    }
+}
