2be84666bc5070160dbbaf638f0602c0e077e183
LanKuDot <airlanser@gmail.com>
2021-05-27 15:22:44 +0800

    Rename ObjectPool folder to ObjectManagement

---
+++ ObjectManagement/Editor/ObjectPoolEditor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+
+namespace LanKuDot.UnityToolBox.ObjectManagement.Editor
+{
+    [CustomEditor(typeof(ObjectPool))]
+    public class ObjectPoolEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            serializedObject.Update();
+            UpdatePoolItemName();
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void UpdatePoolItemName()
+        {
+            var poolItemsProp = serializedObject.FindProperty("_poolItems");
+            var numOfItems = poolItemsProp.arraySize;
+
+            for (var i = 0; i < numOfItems; ++i) {
+                var itemProp = poolItemsProp.GetArrayElementAtIndex(i);
+                var nameProp = itemProp.FindPropertyRelative("name");
+                var obj =
+                    itemProp.FindPropertyRelative("objectToPool").objectReferenceValue;
+
+                if (obj == null)
+                    nameProp.stringValue = "-- Unassigned --";
+                else
+                    nameProp.stringValue = obj.name;
+            }
+        }
+    }
+}
