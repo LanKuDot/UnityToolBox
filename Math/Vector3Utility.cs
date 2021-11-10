c4a85ded10cf4cd30044929495a010556da1efba
LanKuDot <airlanser@gmail.com>
2021-11-10 17:17:20 +0800

    Vector3Utility: Specify the axis to be ignored

--- Math/Vector3Utility.cs
+++ Math/Vector3Utility.cs
@@ -5,14 +5,18 @@ namespace LanKuDot.UnityToolBox.Math
     public static class Vector3Utility
     {
         /// <summary>
-        /// Get the normalized direction from pos to to pos, and ignore the y direction
+        /// Get the normalized direction from pos to to pos
         /// </summary>
         /// <param name="from">The from position</param>
         /// <param name="to">The to position</param>
-        public static Vector3 GetDirectionIgnoreY(Vector3 from, Vector3 to)
+        /// <param name="ignoreAxis">
+        /// The axis to be ignored. The value of the ignored axis should be 1.
+        /// </param>
+        public static Vector3 GetDirection(
+            Vector3 from, Vector3 to, Vector3 ignoreAxis)
         {
             var direction = to - from;
-            direction.y = 0;
+            direction -= Vector3.Scale(direction, ignoreAxis);
             return direction.normalized;
         }
 
