ebc203207c0e2524dd435808dd56a0bacdab6b93
LanKuDot <airlanser@gmail.com>
2021-11-10 17:20:13 +0800

    TweenEaseCurve: Add tween ease end value

--- Others/TweenEaseCurve.cs
+++ Others/TweenEaseCurve.cs
@@ -7,7 +7,7 @@ namespace LanKuDot.UnityToolBox
     /// The ease curve for a tween
     /// </summary>
     [Serializable]
-    public class TweenEaseCurve
+    public class TweenEaseCurve<T>
     {
         [SerializeField]
         [Tooltip("The ease curve")]
@@ -15,8 +15,12 @@ namespace LanKuDot.UnityToolBox
         [SerializeField]
         [Tooltip("The duration of this ease curve")]
         private float _duration;
+        [SerializeField]
+        [Tooltip("The end value of the tween")]
+        private T _endValue;
 
         public AnimationCurve curve => _curve;
         public float duration => _duration;
+        public T endValue => _endValue;
     }
 }
