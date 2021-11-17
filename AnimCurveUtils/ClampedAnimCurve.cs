fb61f7c021322229d1162b36d3a3247c38e2f59b
LanKuDot <airlanser@gmail.com>
2021-11-17 10:59:48 +0800

    Move the AnimationCurve related to the directory

--- AnimCurveUtils/ClampedAnimCurve.cs
+++ AnimCurveUtils/ClampedAnimCurve.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
 
-namespace LanKuDot.UnityToolBox.Math
+namespace LanKuDot.UnityToolBox.AnimCurveUtils
 {
-    public class AnimationCurveHelper
+    public class ClampedAnimCurve
     {
         private readonly AnimationCurve _curve;
         private readonly float _minTime;
         private readonly float _maxTime;
 
-        public AnimationCurveHelper(AnimationCurve curve)
+        public float minTime => _minTime;
+        public float maxTime => _maxTime;
+
+        public ClampedAnimCurve(AnimationCurve curve)
         {
             _curve = curve;
             _minTime = _curve[0].time;
@@ -18,9 +21,9 @@ namespace LanKuDot.UnityToolBox.Math
         /// <summary>
         /// Evaluate the animation curve within the time period specified in the curve
         /// </summary>
-        /// <param name="time">The time</param>
+        /// <param name="time">The time, which will be clamped in the time range</param>
         /// <returns>The value evaluated in the range of the curve</returns>
-        public float EvaluateClamped(float time)
+        public float Evaluate(float time)
         {
             return _curve.Evaluate(Mathf.Clamp(time, _minTime, _maxTime));
         }
