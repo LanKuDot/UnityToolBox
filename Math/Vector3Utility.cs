7a21ec3793ee573299a8c63adc82da19ad1ec5bd
LanKuDot <airlanser@gmail.com>
2021-12-08 13:32:26 +0800

    Vector3Utility: Provide vector and direction calculation

--- Math/Vector3Utility.cs
+++ Math/Vector3Utility.cs
@@ -5,7 +5,23 @@ namespace LanKuDot.UnityToolBox.Math
     public static class Vector3Utility
     {
         /// <summary>
-        /// Get the normalized direction from pos to to pos
+        /// Get the vector between two points with some ignored axis
+        /// </summary>
+        /// <param name="from">The from position</param>
+        /// <param name="to">The to position</param>
+        /// <param name="ignoreAxis">
+        /// The axis to be ignored. The value of the ignored axis should be 1.
+        /// </param>
+        public static Vector3 GetVector(
+            Vector3 from, Vector3 to, Vector3 ignoreAxis = default)
+        {
+            var direction = to - from;
+            direction -= Vector3.Scale(direction, ignoreAxis);
+            return direction;
+        }
+
+        /// <summary>
+        /// Get the normalized direction from pos to to pos with some ignored axis
         /// </summary>
         /// <param name="from">The from position</param>
         /// <param name="to">The to position</param>
@@ -13,11 +29,23 @@ namespace LanKuDot.UnityToolBox.Math
         /// The axis to be ignored. The value of the ignored axis should be 1.
         /// </param>
         public static Vector3 GetDirection(
-            Vector3 from, Vector3 to, Vector3 ignoreAxis)
+            Vector3 from, Vector3 to, Vector3 ignoreAxis = default)
         {
-            var direction = to - from;
-            direction -= Vector3.Scale(direction, ignoreAxis);
-            return direction.normalized;
+            return GetVector(from, to, ignoreAxis).normalized;
+        }
+
+        /// <summary>
+        /// Get the distance between two points with some ignored axis
+        /// </summary>
+        /// <param name="from">The from position</param>
+        /// <param name="to">The to position</param>
+        /// <param name="ignoreAxis">
+        /// The axis to be ignored. The value of the ignored axis should be 1.
+        /// </param>
+        public static float GetDistance(
+            Vector3 from, Vector3 to, Vector3 ignoreAxis = default)
+        {
+            return GetVector(from, to, ignoreAxis).magnitude;
         }
 
         /// <summary>
