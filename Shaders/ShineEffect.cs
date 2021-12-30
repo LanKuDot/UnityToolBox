f4af339203b8fdfb6c6df3da0ec9c330fb2a271a
LanKuDot <airlanser@gmail.com>
2021-12-30 11:21:27 +0800

    Add ShineEffect

---
+++ Shaders/ShineEffect.cs
@@ -0,0 +1,45 @@
+using DG.Tweening;
+using LanKuDot.UnityToolBox.Tween;
+using UnityEngine;
+
+namespace LanKuDot.UnityToolBox.Shaders
+{
+    public class ShineEffect : MonoBehaviour
+    {
+        [SerializeField]
+        private Renderer[] _renderers;
+        [SerializeField]
+        [ColorUsage(true, true)]
+        private Color _startColor;
+        [SerializeField]
+        private TweenHDREaseCurve _shineCurve;
+
+        private MaterialPropertyBlock _materialProperty;
+        private readonly int _emissionColor = Shader.PropertyToID("_EmissionColor");
+        private Color _color;
+
+        private void Awake()
+        {
+            _materialProperty ??= new MaterialPropertyBlock();
+            foreach (var renderer in _renderers)
+                renderer.material.EnableKeyword("_EMISSION");
+        }
+
+        public void Shine()
+        {
+            _color = _startColor;
+            DOTween.To(
+                    () => _color, x => _color = x,
+                    _shineCurve.endValue, _shineCurve.duration)
+                .SetEase(_shineCurve.curve)
+                .OnUpdate(UpdateMaterialColor);
+        }
+
+        private void UpdateMaterialColor()
+        {
+            _materialProperty.SetColor(_emissionColor, _color);
+            foreach (var renderer in _renderers)
+                renderer.SetPropertyBlock(_materialProperty);
+        }
+    }
+}
